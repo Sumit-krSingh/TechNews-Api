41a84dd31c95b6d81dae465fd07b5885f26d6280
Sumit Singh <kr.sumit817@gmail.com>
2023-11-21 22:32:36 +0530

    initial CSS with file

--- src/componemt/Dailyupdate.js
+++ src/componemt/Dailyupdate.js
@@ -1,26 +1,44 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useGlobalContext } from './context'
+import "../App.css";
 
 const Dailyupdate = () => {
 
-    const {hits, isLoading} = useGlobalContext();
+    const { hits, isLoading } = useGlobalContext();
 
-   
 
-    if(isLoading){
-        return(<>
+
+    if (isLoading) {
+        return (<>
             <h2>Loading......</h2>
         </>
         );
     }
     return (
         <>
-            <h2>Technical daily news</h2>
+            <div className="updateBox">
+                <h2>Technical daily news</h2>
 
-            {hits.map((curPost)=>{
-                return <h2>{curPost.title}</h2>
+                {hits.map((curPost) => {
+                    const { title, author, url, num_comments, objectID } = curPost;
+                    return (
+                        <>
+                            <div className="card" key ={objectID}>
+                                <h2>{title}</h2>
+                                <p>
+                                    By <span>{author}</span> |
+                                    <span>{num_comments}</span>comment
+                                </p>
+                                <div className="card-button">
+                                    <a href={url} target='_blank'>Read More</a>
+                                    <a id="remove" href="#">Remove</a>
+                                </div>
+                            </div>
+                        </>
+                    )
 
-            })}
+                })}
+            </div>
         </>
     )
 }
